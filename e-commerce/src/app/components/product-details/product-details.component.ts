4367568844c712a00d82da2e1821981cbd3553f6
ghaith_mlika <mlika.gaith@gmail.com>
2022-11-26 18:56:06 +0100

    add cart service

--- e-commerce/src/app/components/product-details/product-details.component.ts
+++ e-commerce/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from 'src/app/products';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -9,7 +10,10 @@ import { Product, products } from 'src/app/products';
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private cartService: CartService
+  ) {}
   // executes first when the component is rendered
   ngOnInit(): void {
     // extract product id from the current route
@@ -18,5 +22,10 @@ export class ProductDetailsComponent implements OnInit {
 
     // find the corresponding product
     this.product = products.find((product) => product.id === productId);
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added o the cart!');
   }
 }
