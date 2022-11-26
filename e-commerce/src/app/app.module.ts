979dcb6a9ac7e503a8f2b8fa8a15db4e19ccde37
ghaith_mlika <mlika.gaith@gmail.com>
2022-11-26 19:21:35 +0100

    add HttpClienModule to app

--- e-commerce/src/app/app.module.ts
+++ e-commerce/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,12 @@ import { CartComponent } from './components/cart/cart.component';
     ProductDetailsComponent,
     CartComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
